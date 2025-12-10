8d31a89c02979e7c174c111854edba34cc2deba2
Simon <Simon@SBARAKA>
2025-12-10 09:08:01 +0300

    File modified

---
+++ Day_8.js
@@ -0,0 +1,55 @@
+
+/*
+ * OBJECTS IN JAVA SCRIPT 
+ * Objects store data in key-value pairs
+ * Keys are typically strings
+ * Create Objects using {}
+ */
+
+let products = {
+    "name": "Milk",
+    "Type": "Fresh",
+    "Weight": 60,
+    "price": 6000,
+    "BuyDate": new Date("2025-12-11"),
+    "IsSold": true
+}
+
+/*
+*Accessing Elements in an Object sing keys objectName.key
+*/
+
+console.log(products.IsSold)
+console.log(products.Type)
+console.log(products.name)
+
+/*
+    *OBJECT OPERATIONS
+*/
+ products.name = "Meat"
+ products.price = 500
+products.BuyDate = new Date("2026-12-22")
+
+ console.log(products.name)
+ console.log(products.price)
+ console.log(products.BuyDate)
+ console.log(products)
+
+ //deleting a key
+ delete products.BuyDate 
+ console.log(products)
+
+ /*
+    *LOOPING THROUGH OBJECT 
+*/
+
+for (const product in products){
+    // accesses values 
+    console.log(products[product])
+}
+
+//Helps access keys 
+console.log(Object.keys(products))
+
+//Accessing the entries or Key-value Pairs
+console.log(Object.entries(products))
